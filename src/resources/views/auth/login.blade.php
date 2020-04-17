b0d7dc5a45bf8dfa125e83a73eaa306209da6c46
Maros Jasan <maros.jasan@gmail.com>
2020-04-17 18:06:27 +0200

    login view improvements

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -21,11 +21,7 @@
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
 
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
+                                @include('assistant::validation.field', ['field' => 'email'])
                             </div>
                         </div>
 
@@ -35,11 +31,7 @@
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
 
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
+                                @include('assistant::validation.field', ['field' => 'password'])
                             </div>
                         </div>
 
