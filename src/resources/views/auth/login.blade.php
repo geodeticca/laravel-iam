08bbecc6bb09082fe1a6d00ce80abfa3dc37536d
Marek Jasan <mjasan@geodeticca.sk>
2020-09-28 11:10:53 +0200

    login view improvements

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -16,10 +16,10 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Login') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+                                <input id="email" type="text" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
 
                                 @include('assistant::validation.field', ['field' => 'email'])
                             </div>
