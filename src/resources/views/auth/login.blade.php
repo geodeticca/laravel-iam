ba55ed35cb325f6c7f4e151ff70e5236ce6f3f9b
Marek Jasan <mjasan@geodeticca.sk>
2020-09-29 09:24:58 +0200

    login form improvementws

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -16,12 +16,12 @@
                             @csrf
 
                             <div class="form-group row">
-                                <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('iam::account.username') }}</label>
+                                <label for="login" class="col-md-4 col-form-label text-md-right">{{ __('iam::account.username') }}</label>
 
                                 <div class="col-md-6">
-                                    <input id="email" type="text" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+                                    <input id="login" type="text" class="form-control{{ $errors->has('login') ? ' is-invalid' : '' }}" name="login" value="{{ old('login') }}" required autofocus>
 
-                                    @include('assistant::validation.field', ['field' => 'email'])
+                                    @include('assistant::validation.field', ['field' => 'login'])
                                 </div>
                             </div>
 
