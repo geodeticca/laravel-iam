744671c5d85a2c965161167b13ca4a9a69c462e8
Maros Jasan <maros.jasan@gmail.com>
2021-05-12 17:11:45 +0200

    routex fix

--- src/routes/iam.php
+++ src/routes/iam.php
@@ -1,16 +1,5 @@
 <?php
 
-//use Illuminate\Support\Facades\Route;
-//use Illuminate\Support\Facades\Auth;
-
-Route::mixin(new \Laravel\Ui\AuthRouteMethods());
-
-Auth::routes([
-    'register' => false,
-    'verify' => false,
-    'reset' => false,
-]);
-
 Route::namespace('App\Http\Controllers\Iam')->middleware(['web', 'auth'])->group(function () {
 
     // account
