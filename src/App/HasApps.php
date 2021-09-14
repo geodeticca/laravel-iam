96fdff9b572c50c0407f2d00807b610558cb280a
Marek Jasaň <mjasan@geodeticca.sk>
2021-09-14 14:29:10 +0200

    HasApps trait improvements

--- src/App/HasApps.php
+++ src/App/HasApps.php
@@ -16,12 +16,28 @@ trait HasApps
     public array $apps = [];
 
     /**
+     * @var array
+     */
+    public array $app_uniqids = [];
+
+    /**
      * @param int $appId
      * @return $this
      */
     public function addApp(int $appId): self
     {
         $this->apps[] = $appId;
+
+        return $this;
+    }
+
+    /**
+     * @param string $appUniqid
+     * @return $this
+     */
+    public function addAppUniqid(string $appUniqid): self
+    {
+        $this->app_uniqids[] = $appUniqid;
 
         return $this;
     }
@@ -35,12 +51,31 @@ trait HasApps
     }
 
     /**
-     * @param array $apps
+     * @return array
+     */
+    public function getAppUniqids(): array
+    {
+        return array_values(array_unique($this->app_uniqids));
+    }
+
+    /**
+     * @param array $appIds
      * @return $this
      */
-    public function setApps(array $apps): self
+    public function setApps(array $appIds): self
     {
-        $this->apps = array_values(array_unique(array_map('intval', $apps)));
+        $this->apps = array_values(array_unique(array_map('intval', $appIds)));
+
+        return $this;
+    }
+
+    /**
+     * @param array $appUniqids
+     * @return $this
+     */
+    public function setAppUniqids(array $appUniqids): self
+    {
+        $this->app_uniqids = array_values(array_unique($appUniqids));
 
         return $this;
     }
